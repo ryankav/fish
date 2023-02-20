aa4a8e66111bd576ab7d3408f87ce7ed553530ca
Ryan Kavanagh <ryan.p.kav@gmail.com>
2023-02-20 19:46:24 +0000

    Added starting point for git config

---
+++ conf.d/git.fish
@@ -0,0 +1,5 @@
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate ''
+set __fish_git_prompt_showupstream 'none'
+set -g fish_prompt_pwd_dir_length 3
